08c72a8337b8e198be77931bb858f515c75aa16d
Aman Menda <92232342+AmanMenda@users.noreply.github.com>
2023-04-05 14:27:30 +0100

    Update my_strlen.c

--- my_strlen.c
+++ my_strlen.c
@@ -10,8 +10,7 @@ int my_strlen(char const *str)
     int a = 0;
     int b = 0;
 
-    for (a = 0; str[a] != '\0'; a++) {
-        b++;
-    }
+    for(a= 0; str[a] != '\0'; a++) {
+        b++;}
     return (b);
 }
